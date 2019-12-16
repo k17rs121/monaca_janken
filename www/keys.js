c308e2a19d0374fd9cd291484317f10ef35152e1
k17rs121 <k17rs121@st.kyusan-u.ac.jp>
2019-12-16 14:26:54 +0900

    backend登録

--- www/keys.js
+++ www/keys.js
@@ -1,3 +1,3 @@
 // This is a JavaScript file
-var appKey = "";
-var clientKey = "";
+var appKey = "022c4f9c19cea0a1086a0fda18d8d5073b7a734295f733e10ff702e9e8a3471b";
+var clientKey = "50f0d19306ed60ca1bc99ec394a44c9b30e11b15e016a8fc32bb8053bd65f834";
